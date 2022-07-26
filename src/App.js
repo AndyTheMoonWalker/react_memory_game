2ad60848451c05e975f69d10e1f878dd1b532cc6
Andy <none>
2022-07-26 13:33:15 +0200

    cards object added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+const cardImages = [
+	{ src: '/img/helmet-1.png' },
+	{ src: '/img/potion-1.png' },
+	{ src: '/img/ring-1.png' },
+	{ src: '/img/scroll-1.png' },
+	{ src: '/img/shield-1.png' },
+	{ src: '/img/sword-1.png' },
+];
 
 function App() {
-	return <div className='App'></div>;
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
+
+	const shuffle = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+		setCards(shuffledCards);
+		setTurns(0);
+	};
+
+	console.log(cards, turns);
+	return (
+		<div className='App'>
+			<h1>Memory game</h1>
+			<button onClick={shuffle}>Start</button>
+		</div>
+	);
 }
 
 export default App;
